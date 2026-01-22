db6e89cc8171878169d0cd628dff1d87f7d0eb81
AttilioFintech <attilio.infante@icloud.com>
2026-01-22 09:41:38 +0100

    Update metrics.py

--- core/metrics.py
+++ core/metrics.py
@@ -1,1 +1,9 @@
+def calculate_savings_rate(income, expenses):
+    if income <= 0:
+        return 0
+    return (income - expenses) / income * 100
 
+
+def calculate_emergency_months(emergency_fund, monthly_expenses):
+    if monthly_expenses <= 0:
+        return 0
