2390a5662a0763e08839a7efde102cb4f17225e2
AttilioFintech <attilio.infante@icloud.com>
2026-01-22 09:46:16 +0100

    Update trajectory.py

--- core/trajectory.py
+++ core/trajectory.py
@@ -1,1 +1,7 @@
+def project_savings(current_savings, monthly_delta, months=12):
+    projections = []
+    value = current_savings
 
+    for _ in range(months):
+        value += monthly_delta
+        projections.append(value)
